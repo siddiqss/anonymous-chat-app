f3162e91338e5f1b47c874739762603d4de40f5d
Talha <tstalhasiddique@gmail.com>
2019-07-19 23:40:37 +0500

    Build Files Added

---
+++ build/precache-manifest.ed2a3ead12672c740dd13be803188bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b23c97efff8740a9d57d7be60be499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1a4dac00ecb74ddeaab",
+    "url": "/static/css/main.8b573827.chunk.css"
+  },
+  {
+    "revision": "8a27e7352ce470f19164",
+    "url": "/static/js/2.fb4d59c7.chunk.js"
+  },
+  {
+    "revision": "c1a4dac00ecb74ddeaab",
+    "url": "/static/js/main.1e480423.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
